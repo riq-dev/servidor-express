d50c473b4daaa4dad8876ff874e9d427636035c7
riq-dev <luis.riq.henrique@gmail.com>
2023-07-30 06:40:37 -0400

    update

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`Request Type: ${req.method}`);
+    console.log(`Content Type: ${req.headers['content-type']}`);
+    console.log(`Date: ${new Date()}`);
+
+    next();
+});
+
 app.get('/users', async (req, res) => {
     try {
         const users = await UserModel.find({});
