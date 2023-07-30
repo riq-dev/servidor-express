78cb136d354d58f3b8c7d8b9ccb9bc9e09d9b350
riq-dev <luis.riq.henrique@gmail.com>
2023-07-30 18:25:23 -0400

    update

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 
+app.set('view engine', 'ejs')
+app.set('views', 'src/views');
+
 app.use((req, res, next) => {
     console.log(`Request Type: ${req.method}`);
     console.log(`Content Type: ${req.headers['content-type']}`);
@@ -12,6 +15,11 @@ app.use((req, res, next) => {
 
     next();
 });
+
+app.get('/views/users', async (req, res) => {
+    const users = await UserModel.find({});
+    res.render('index', { users });
+})
 
 app.get('/users', async (req, res) => {
     try {
